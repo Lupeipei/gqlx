c94a360ac3bc7356cd90763239c97853f04b1422
lupeipei <learningleadtorebirth@gmail.com>
2019-10-15 10:35:38 +0800

    add human helper methods for i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+protected
+  def human(model, attribute = nil)
+    attribute ? model.human_attribute_name(attribute) : model.model_name.human
+  end
+  alias h human
+  helper_method :human, :h
 end
