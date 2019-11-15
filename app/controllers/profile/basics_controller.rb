ec270f88a184f0b2e868da5c2f76d05bc9c6320a
lupeipei <learningleadtorebirth@gmail.com>
2019-11-15 17:47:14 +0800

    change basic profile edit page

--- app/controllers/profile/basics_controller.rb
+++ app/controllers/profile/basics_controller.rb
@@ -1,10 +1,17 @@
 class Profile::BasicsController < ApplicationController
   layout 'backend'
 
-  def show
+  def edit
     @admin = current_admin
   end
 
   def update
+    @admin = current_admin
+    @admin.update(admin_params)
+    respond_with @admin
+  end
+protected
+  def admin_params
+    params.fetch(:admin).permit(:name, :password, :password_confirmation)
   end
 end
