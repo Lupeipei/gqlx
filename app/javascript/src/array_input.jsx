7b99f3f23daf0f492dd91a7e1514b2f4f1a1454b
lucia <luciacachn@gmail.com>
2020-07-18 16:11:49 +0800

    rewrite array input component

--- app/javascript/src/array_input.jsx
+++ app/javascript/src/array_input.jsx
@@ -1,19 +1,21 @@
 import $ from 'jquery';
 
-$(document).on('content:loaded', function(event) {
-  $(event.target).find('.array-add-item').each(function() {
-    $(this).on('click', e => {
-      e.preventDefault()
-      const currentLineField = this.parentElement.parentElement
-      const $cloneField = $(currentLineField).clone()
-      $cloneField.find('input').val("")
-      $cloneField.find('button').removeAttr('disabled')
-      $(currentLineField).after($cloneField)
-    })
+const initArrayInput = function(root) {
+  $(root).on('click', '.array-add-item', e => {
+    e.preventDefault()
+    const currentLineField = e.currentTarget.parentElement.parentElement
+    const $cloneField = $(currentLineField).clone()
+    $cloneField.find('input').val("")
+    $cloneField.find('button').removeAttr('disabled')
+    $(currentLineField).after($cloneField)
   })
 
-  $(event.target).on('click', '.array-remove-item', e => {
+  $(root).on('click', '.array-remove-item', e => {
     e.preventDefault()
     e.currentTarget.parentElement.parentElement.remove()
   })
+}
+
+$(document).on('content:loaded', function(event) {
+  initArrayInput(event.target)
 })
